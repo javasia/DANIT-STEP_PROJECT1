6438b422d2cc78528f5a195aff3f13c6a8aa4716
javasia <impex.ua@gmail.com>
2018-09-09 23:09:28 +0300

    step

--- ham/js/script.js
+++ ham/js/script.js
@@ -7,3 +7,13 @@ document.querySelector("#search").addEventListener('blur', function () {
     event.target.style.display = "none";
     document.querySelector("#search-img").style.display = "block";
 });
+
+document.querySelector("#search-link").addEventListener('click', () => event.preventDefault());
+
+$(window).on("load", function () {
+    $(".grid").masonry({
+        columnWidth: '.grid-item',
+        itemSelector: '.grid-item'
+    });
+});
+
