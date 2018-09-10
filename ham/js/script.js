5763abbfddba8b05bae3a0fa021639856b60d01b
javasia <impex.ua@gmail.com>
2018-09-10 23:27:09 +0300

    step

--- ham/js/script.js
+++ ham/js/script.js
@@ -13,7 +13,7 @@ document.querySelector("#search-link").addEventListener('click', () => event.pre
 
 $(window).on("load", function () {
     $("#grid").masonry({
-        columnWidth: 10, 
+        columnWidth: 123, 
         itemSelector: '.grid-item'
     });
 });
