612ae493074a605efce6981e0d146931d01824fc
javasia <impex.ua@gmail.com>
2018-09-13 20:54:57 +0300

    step

--- ham/js/script.js
+++ ham/js/script.js
@@ -3,24 +3,22 @@ document.addEventListener('DOMContentLoaded', function(){
 	    document.switchDisplay(event.target, false);
 	    document.switchDisplay(document.querySelector("#search"), true);
 	});
-
 	document.querySelector("#search").addEventListener('blur', function () {
     	document.switchDisplay(event.target, false);
     	document.switchDisplay(document.querySelector("#search-img"), true);
 	});
-
 	document.querySelector("#search-link").addEventListener('click', () => event.preventDefault());
-	
 	document.htmlCollectionToArray(document.querySelector("#workpics-switch-menu").children).forEach(button => button.addEventListener('click', filterCategories));
-
-// document.querySelector("#amazing-work-add-button").addEventListener("click", loadmore);
-//  document.querySelector("#amazing-work-add-button").addEventListener("click", loadingAnimate);
-   document.querySelector("#amazing-work-add-button").addEventListener("click", loadmoreAnimated);
+    document.querySelector("#amazing-work-add-button").addEventListener("click", loadmoreAnimated);
 });
 
 $(window).on("load", function () {
-    $("#grid").masonry({
-        columnWidth: 130, 
-        itemSelector: '.grid-item'
-    });
+	$("#grid").masonry({
+	    itemSelector: ".grid-item",
+    	columnWidth: 10,
+    	gutter: 11,
+    	fitWidth: true,
+	});
 });
+
+
