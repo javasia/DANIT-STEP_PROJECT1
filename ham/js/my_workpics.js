e7495f6afa4f258aef7888854b1dd573acca2248
javasia <impex.ua@gmail.com>
2018-09-13 23:33:16 +0300

    step

--- ham/js/my_workpics.js
+++ ham/js/my_workpics.js
@@ -16,37 +16,39 @@ function getWorkPicNodesSet() {
 
 function loadmoreAnimated() {
     event.preventDefault();
-    loadingAnimate.call(event, loadmore);
+    animateFunction.call(event, loadmore);
 }
 
 function loadmore() {
     if (event) {
-        
-    } else {
-        document.switchDisplay(this.target.parentElement, false);
+        return loadmore.call(event);
+    }
+    let parent = document.querySelector("#workpics");
+    if (parent.maxPages > parent.currentPage) { // protection from idiots
         resetFilter();
-        let fileName = "img/amazing/extra/%NUMBER%.jpg";
-        let parent = document.querySelector("#workpics");
+        let fileName = "img/amazing/img%NUMBER%.jpg";
         let model = parent.firstElementChild;
         let categories = document.htmlCollectionToArray(document.querySelector("#workpics-switch-menu").children).map(el => el.textContent);
-        for (var i = 0; i < 12; i++) {
+        for (var i = 1; i <= 12; i++) {
             let containerNode = document.createElement("div");
             containerNode.classList = model.classList;
             containerNode.setAttribute("category", categories[Math.round(Math.random() * 3 + 1)]);
             containerNode.innerHTML = model.innerHTML;
             let imgNodeEl = document.createElement("img");
-            imgNodeEl.setAttribute("src", fileName.replace("%NUMBER%", i + 1));
+            imgNodeEl.setAttribute("src", fileName.replace("%NUMBER%", i + parent.picsPerPage * parent.currentPage));
             containerNode.replaceChild(imgNodeEl, containerNode.lastElementChild);
             parent.appendChild(containerNode);
         }
+        parent.currentPage++;
     }
+    document.switchDisplay(this.target.parentElement, parent.maxPages > parent.currentPage)
 }
 
 function resetFilter() {
     getWorkPicNodesSet().forEach((node) => document.switchDisplay(node, true));
 }
 
-function loadingAnimate(funtionToRun) {
+function animateFunction(funtionToRun) {
     let animation = document.querySelector(".loading-animation");
     let buttonContainer = this.target.parentElement;
     document.switchDisplay(buttonContainer, false);
--- ham/js/script.js
+++ ham/js/script.js
@@ -9,15 +9,19 @@ document.addEventListener('DOMContentLoaded', function(){
 	});
 	document.querySelector("#search-link").addEventListener('click', () => event.preventDefault());
 	document.htmlCollectionToArray(document.querySelector("#workpics-switch-menu").children).forEach(button => button.addEventListener('click', filterCategories));
+    document.querySelector("#workpics").maxPages = 3;
+    document.querySelector("#workpics").currentPage = 1;
+    document.querySelector("#workpics").picsPerPage = 12;
     document.querySelector("#amazing-work-add-button").addEventListener("click", loadmoreAnimated);
 });
 
 $(window).on("load", function () {
 	$("#grid").masonry({
 	    itemSelector: ".grid-item",
-    	columnWidth: 10,
-    	gutter: 11,
-    	fitWidth: true,
+    	columnWidth: 63,
+    	gutter: 1,
+        fitWidth: true,
+        horizontalOrder: true
 	});
 });
 
